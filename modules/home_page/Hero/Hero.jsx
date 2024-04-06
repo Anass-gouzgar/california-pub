23697799f992b9d04408e53b339ee10069ec88c0
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-04-06 15:14:15 +0000

    fixed the hero img

--- modules/home_page/Hero/Hero.jsx
+++ modules/home_page/Hero/Hero.jsx
@@ -39,10 +39,10 @@ const Hero = () => {
         </div>
       </div>
       {/* machine image */}
-      <div className="hidden md:flex h-[80%] bg-yellow-300  rounded-xl ovetrflow-hidden">
+      <div className="hidden md:flex h-[80%] bg-yeljlow-300 pt-14 rounded-xl ovetrflow-hidden">
         <Image
           alt="machine cnc"
-          src={coc}
+          src={img}
           className="w-full h-auto object-cover "
           loading="lazy"
         />
