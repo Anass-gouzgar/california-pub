2d73eb315a6613aa6a100b4733b1131dfc3ed864
anass-gouzgar <go.anass.gouzgar@gmail.com>
2024-04-06 04:00:14 +0000

    Added the wtsp floating icon

---
+++ modules/home_page/Whats_App_Icon/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import img from "@/public/assets/images/whatsAppIcon.png";
+
+
+const WhatsAppIcon = () => {
+  return (
+    <div className="fixed bottom-4 right-4 z-50 animate-float ">
+      <Link
+        href="https://api.whatsapp.com/send/?phone=%2B212768049725&text&type=phone_number&app_absent=0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image src={img} alt="WhatsApp" className="w-16 h-auto" />
+      </Link>
+    </div>
+  );
+};
+
+export default WhatsAppIcon;
